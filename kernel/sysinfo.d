ecc58c19771de7a32991472d54f1dc9691cbd13c
yswdra <945394722@qq.com>
2022-01-17 23:53:56 +0800

    init pgtbl code files && some parts of lab3 work update

---
+++ kernel/sysinfo.d
@@ -0,0 +1,3 @@
+kernel/sysinfo.o: kernel/sysinfo.c kernel/types.h kernel/param.h \
+ kernel/memlayout.h kernel/riscv.h kernel/spinlock.h kernel/proc.h \
+ kernel/syscall.h kernel/defs.h kernel/sysinfo.h
